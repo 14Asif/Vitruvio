0d06986ef0d622656c79ec81de1e9efeef613629
Asifali <asifalisayed19@gmail.com>
2020-02-21 07:20:55 +0000

    initial test commit

---
+++ precache-manifest.b2911b1763da71e6353be05b9fdaa2ba.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e035def1ca5d8166db50d4116ad64826",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddb5e31ad12bf7d357cf",
+    "url": "/static/css/2.42e056c6.chunk.css"
+  },
+  {
+    "revision": "8ea4eb849185eccd8b00",
+    "url": "/static/css/main.57463685.chunk.css"
+  },
+  {
+    "revision": "ddb5e31ad12bf7d357cf",
+    "url": "/static/js/2.9b2d61e7.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.9b2d61e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ea4eb849185eccd8b00",
+    "url": "/static/js/main.424b32ea.chunk.js"
+  },
+  {
+    "revision": "c2315dc8b83aa6cd4d53",
+    "url": "/static/js/runtime-main.63c38c94.js"
+  },
+  {
+    "revision": "670bc3514b7e60509b69033dcd9e41e7",
+    "url": "/static/media/bannar-img-2.670bc351.png"
+  },
+  {
+    "revision": "6a9921dbfcafc2314c305478f37d4e06",
+    "url": "/static/media/brooke.6a9921db.jpg"
+  },
+  {
+    "revision": "2416d891a428211e0cf6d80ef94e75c8",
+    "url": "/static/media/chat.2416d891.png"
+  },
+  {
+    "revision": "024d9fcbc892e18c98830b844503b2e6",
+    "url": "/static/media/clinton1.024d9fcb.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "c30bc1f34c24d393dc809ae9929e9c90",
+    "url": "/static/media/img2.c30bc1f3.jpg"
+  },
+  {
+    "revision": "34a9f4411a7809021429c6130ce016d4",
+    "url": "/static/media/img3.34a9f441.jpg"
+  },
+  {
+    "revision": "75bf7d6b111b1f3745dbc66e3d6f05f1",
+    "url": "/static/media/img4.75bf7d6b.jpg"
+  },
+  {
+    "revision": "160c9ffb0bccaa821c4477233df450f8",
+    "url": "/static/media/img5.160c9ffb.jpg"
+  },
+  {
+    "revision": "3bab0044d2b256cc51e8a20e67e382f8",
+    "url": "/static/media/medicalstory.3bab0044.png"
+  }
+]);
